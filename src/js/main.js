7bf377b89789521639ae491be53ece2329e61ae8
Xinyu Zhang <beviszhang1993@gmail.com>
2015-05-24 16:03:57 -0700

    drag drop

--- src/js/main.js
+++ src/js/main.js
@@ -21,10 +21,17 @@ function main() {
 
 (function() {
     var nop = function() {};
+    var $apply = function($scope) {
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
+    };
+
 
     var app = Globals.app = angular.module('me310_Table', ['ionic',
-                                                           'wu.masonry',
-                                                           'ngDraggable']);
+        'wu.masonry',
+        'ngDraggable'
+    ]);
 
     var message = {};
     message.startUp = function() {
@@ -136,130 +143,180 @@ function main() {
 
     if (!Constant.DEBUG_UI) {
         app.controller('AppCtrl',
-                       function($scope, storage_path, pasteasy_qrcode, $ionicModal, $ionicPopover) {
-            // Every Thing Goes Here
-            storage_path.then(function(storage_path) {
-                pasteasy_qrcode.then(function(pasteasy_qrcode) {
+                       function($scope, $log, storage_path, pasteasy_qrcode, $ionicModal, $ionicPopover,$timeout) {
 
-                    // setup $scope variables
-                    SetUpGallery($scope);
+                // Every Thing Goes Here
+                storage_path.then(function(storage_path) {
+                    pasteasy_qrcode.then(function(pasteasy_qrcode) {
 
-                    /**
-                     lexicon_input
-                     main_gallery
-                     lexicon_images
-                     lexicon_words
-                     */
-                    // setup the dirwatcher
-                    var dir_watcher = new Class.DirWatcher(storage_path, function(path, stat) {
-                        if (Util.isImage(path))
-                            $scope.main_gallery.push(path);
-                    });
+                        var set = $scope.set = function(k, v) {
+                            $scope[k] = v;
+                            return set;
+                        };
+                        var get = $scope.get = function(k) {
+                            return $scope[k];
+                        };
+                        // setup $scope variables
+                        SetUpGallery($scope);
 
-                    // start the server
-                    var sharing_server = Util.createSharingServer({
-                        Util: Util,
-                        port: 3000,
-                        path: storage_path
-                    });
+                        /**
+                         lexicon_input
+                         main_gallery
+                         lexicon_images
+                         lexicon_words
+                         */
+                        // setup the dirwatcher
+                        var dir_watcher = new Class.DirWatcher(storage_path, function(path, stat) {
+                            if (Util.isImage(path))
+                                $scope.main_gallery.unshift(path);
+                            $apply($scope);
+                        });
+
+                        // start the server
+                        var sharing_server = Util.createSharingServer({
+                            Util: Util,
+                            port: 3000,
+                            path: storage_path
+                        });
 
 
-                    dir_watcher.start();
-                    sharing_server.start();
+                        dir_watcher.start();
+                        sharing_server.start();
 
 
-                    // Watching the lexicon_input
-                    var latest_search_input;
-                    $scope.$watch('lexicon_input', function(nv, ov) {
-                        latest_search_input = nv;
-                        if (nv) {
-                            Util.getRelatedWord(Util.tokenizeAndStem(nv), function(err, origin_value, resultList) {
-                                if (err)
-                                    throw err;
-                                else {
-                                    if (origin_value == latest_search_input) {
-                                        var result_to_display = [];
-                                        resultList.forEach(function(r) {
-                                            result_to_display = result_to_display.concat(r.slice(0, 10));
-                                        });
-                                        $scope.lexicon_words = result_to_display.slice(0, 10);
-                                        if (!$scope.$$phase) {
-                                            $scope.$apply();
+                        // Watching the lexicon_input
+                        var latest_search_input;
+                        $scope.$watch('lexicon_input', function(nv, ov) {
+                            latest_search_input = nv;
+                            if (nv) {
+                                Util.getRelatedWord(Util.tokenizeAndStem(nv), function(err, origin_value, resultList) {
+                                    if (err)
+                                        throw err;
+                                    else {
+                                        if (origin_value == latest_search_input) {
+                                            var result_to_display = [];
+                                            resultList.forEach(function(r) {
+                                                result_to_display = result_to_display.concat(r.slice(0, 10));
+                                            });
+                                            $scope.lexicon_words = result_to_display.slice(0, 10);
+                                            if (!$scope.$$phase) {
+                                                $scope.$apply();
+                                            }
                                         }
                                     }
-                                }
-                            });
+                                });
 
-                            Util.googleImageSearch({
-                                for: nv,
-                                page: Constant.GoogleImagePageNumber,
-                                rsz: Constant.GoogleImageNumberPerPage
-                            }, function(err, origin_value, images) {
-                                if (err) {
-                                    throw err;
-                                } else {
-                                    if (origin_value == latest_search_input) {
-                                        $scope.lexicon_images = images.map(function(img) {
-                                            return img.url;
-                                        });
-                                        $scope.$apply();
+                                Util.googleImageSearch({
+                                    for: nv,
+                                    page: Constant.GoogleImagePageNumber,
+                                    rsz: Constant.GoogleImageNumberPerPage
+                                }, function(err, origin_value, images) {
+                                    if (err) {
+                                        throw err;
+                                    } else {
+                                        if (origin_value == latest_search_input) {
+                                            $scope.lexicon_images = images.map(function(img) {
+                                                return img.url;
+                                            });
+                                            $apply($scope);
+                                        }
                                     }
+                                });
+                            } else {
+                                $scope.lexicon_images = [];
+                                $scope.lexicon_images_cursor = 0;
+                                $scope.lexicon_words = 0;
+                            }
+                        });
+
+                        // Image Modal
+                        $scope.openImageModal = function(src) {
+                            $scope.image_modal_src = src;
+
+                            $scope.openImageModal.modal.show();
+                        };
+                        $scope.closeImageModal = function() {
+                            $scope.openImageModal.modal.hide();
+                        };
+                        $ionicModal.fromTemplateUrl('image-modal.html', function(modal) {
+                            $scope.openImageModal.modal = modal;
+                        }, {
+                            scope: $scope,
+                            animation: 'slide-in-up'
+                        });
+
+
+                        // Qrcode Popover
+                        $scope.openQrPopover = function($event) {
+                            $scope.openQrPopover.popover.show($event);
+                        };
+                        $scope.closeQrPopover = function() {
+                            $scope.openQrPopover.popover.hide();
+                        };
+                        $ionicPopover.fromTemplateUrl('qrcode-popover.html', {
+                            scope: $scope
+                        }).then(function(popover) {
+                            $scope.openQrPopover.popover = popover;
+                        });
+                        $scope.pasteasy_qrcode = pasteasy_qrcode;
+                        $scope.server_qrcode = Util.qrEncode(sharing_server.upload_addr);
+
+
+                        // Drag And Drop
+                        $scope.imageDropTo = function($data, where, deleted) {
+                            $log.info('Dropping');
+                            if ($data) {
+                                if ($data.from == where) {
+                                    $log.info('Drag Drop in the same place');
+                                    return;
+                                } else if ($data.to == where) {
+                                    // data.index;
+                                    $data.index = $data.from.indexOf($data.image);
+                                    if (!deleted) {
+                                        var img = $data.from[$data.index];
+                                        $data.to.unshift(img);
+                                    }
+                                    $data.from.splice($data.index, 1);
+                                    $log.info('Drag Drop Successfully');
                                 }
-                            });
-                        } else {
-                            $scope.lexicon_images = [];
-                            $scope.lexicon_images_cursor = 0;
-                            $scope.lexicon_words = 0;
-                        }
+                            }
+                        };
+
+                        // draggable:end
+                        $scope.dragging_from_gallery = false;
+                        $scope.dragging_from_lexicon = false;
+                        var dragging = function(from, b) {
+                            if (from == $scope.main_gallery) {
+                                $log.info('dragging_from_gallery', b);
+                                set('dragging_from_gallery', b);
+                            } else if (from == $scope.lexicon_images) {
+                                $log.info('dragging_from_lexicon', b);
+                                set('dragging_from_lexicon', b);
+                            }
+                            $apply($scope);
+                        };
+                        $scope.$on('draggable:start', function($event, info) {
+                            dragging(info.data.from, true);
+                        });
+                        $scope.$on('draggable:end', function($event, info) {
+
+                            dragging(info.data.from, false);
+
+                        });
+
+
+                        //rotate
+                        var rotate_hash = {};
+                        $scope.rotate = function() {
+
+                        };
+
+
+                        // END
+                        message.startUp();
                     });
-
-                    // Image Modal
-                    $scope.openImageModal = function(src) {
-                        $scope.image_modal_src = src;
-
-                        $scope.openImageModal.modal.show();
-                    };
-                    $scope.closeImageModal = function() {
-                        $scope.openImageModal.modal.hide();
-                    };
-                    $ionicModal.fromTemplateUrl('image-modal.html', function(modal) {
-                        $scope.openImageModal.modal = modal;
-                    }, {
-                        scope: $scope,
-                        animation: 'slide-in-up'
-                    });
-
-
-                    // Qrcode Popover
-                    $scope.openQrPopover = function($event) {
-                        $scope.openQrPopover.popover.show($event);
-                    };
-                    $scope.closeQrPopover = function(){
-                        $scope.openQrPopover.popover.hide();
-                    };
-                    $ionicPopover.fromTemplateUrl('qrcode-popover.html', {
-                        scope: $scope
-                    }).then(function(popover) {
-                        $scope.openQrPopover.popover = popover;
-                    });
-                    $scope.pasteasy_qrcode = pasteasy_qrcode;
-                    $scope.server_qrcode = Util.qrEncode(sharing_server.upload_addr);
-
-
-                    // Drag And Drop
-                    $scope.imageDrop = function(images, $data,$event){
-                        console.log('from:',$data);
-                        console.log('to:',$event.event.target.src);
-                    };
-
-
-
-
-                    // END
-                    message.startUp();
                 });
             });
-        });
 
     } else {
 
